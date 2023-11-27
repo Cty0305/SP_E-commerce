561e4cad1bc652208e52a007551748cf8d748073
alanchou0305 <mickeychou375@gmail.com>
2023-11-27 10:01:41 +0800

    11/26 update homepage filter js

--- JS/all.js
+++ JS/all.js
@@ -6,3 +6,28 @@ document.addEventListener("DOMContentLoaded", function () {
     headerUser.classList.toggle("show");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const filterToggleWomen = document.getElementById("filter-toggle-women");
+  const filterToggleMen = document.getElementById("filter-toggle-men");
+
+  filterToggleWomen.addEventListener("click", toggleInactiveState);
+  filterToggleMen.addEventListener("click", toggleInactiveState);
+
+  function toggleInactiveState() {
+    filterToggleWomen.classList.toggle("inactive");
+    filterToggleMen.classList.toggle("inactive");
+  }
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const filterSort = document.getElementById("filte-sort");
+//   const filterSortMenu = document.querySelector(".filter-sort-menu");
+
+//   filterSort.addEventListener("click", function () {
+//     filterSortMenu.classList.toggle("filter-sort-menu-show");
+//   });
+// });
+$(".filter-sort").click(function () {
+  $(".filter-sort-menu").toggleClass("filter-sort-menu-show");
+});
