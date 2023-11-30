d33b63f0be3b06e3a6a4b57795e916b783b04abc
alanchou0305 <mickeychou375@gmail.com>
2023-11-30 22:58:33 +0800

    11.30 Update Homepage filter-sort js

--- JS/all.js
+++ JS/all.js
@@ -7,6 +7,25 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// Filter-Sort 下拉選單
+$(".filter-sort").click(function () {
+  $(".filter-sort-menu").toggleClass("filter-sort-menu-show");
+});
+
+$(document).on("click", function (event) {
+  if (!$(event.target).closest(".filter-sort-controller").length) {
+    $(".filter-sort-menu").removeClass("filter-sort-menu-show");
+  }
+});
+
+$(".filter-sort-option").click(function (e) {
+  e.preventDefault();
+  var optionText = $(this).text();
+  $(".filter-sort-text").text(optionText);
+  $(".filter-sort-menu").removeClass("show");
+});
+
+// Filter-toggle-sex
 document.addEventListener("DOMContentLoaded", function () {
   const filterToggleWomen = document.getElementById("filter-toggle-women");
   const filterToggleMen = document.getElementById("filter-toggle-men");
@@ -18,16 +37,4 @@ document.addEventListener("DOMContentLoaded", function () {
     filterToggleWomen.classList.toggle("inactive");
     filterToggleMen.classList.toggle("inactive");
   }
-});
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const filterSort = document.getElementById("filte-sort");
-//   const filterSortMenu = document.querySelector(".filter-sort-menu");
-
-//   filterSort.addEventListener("click", function () {
-//     filterSortMenu.classList.toggle("filter-sort-menu-show");
-//   });
-// });
-$(".filter-sort").click(function () {
-  $(".filter-sort-menu").toggleClass("filter-sort-menu-show");
 });
