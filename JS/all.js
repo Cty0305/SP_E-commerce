f3e3fe54ad86500944d2e55947002c3d820e62cc
alanchou0305 <mickeychou375@gmail.com>
2023-12-03 01:09:11 +0800

    12/3 add owl-carousel on product page

--- JS/all.js
+++ JS/all.js
@@ -55,3 +55,21 @@ $(".add-to-cart").click(function () {
     });
   }
 });
+
+$(".owl-carousel").owlCarousel({
+  loop: true, // 循環播放
+  margin: 12, // 外距 10px
+  nav: true, // 顯示點點
+  dots: false,
+  responsive: {
+    0: {
+      items: 1, // 螢幕大小為 0~600 顯示 1 個項目
+    },
+    600: {
+      items: 3, // 螢幕大小為 600~1000 顯示 3 個項目
+    },
+    1000: {
+      items: 3.5, // 螢幕大小為 1000 以上 顯示 5 個項目
+    },
+  },
+});
